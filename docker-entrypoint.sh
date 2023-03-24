3681201a33d18f56864f9aec7ef65cc049ca7e8c
jonny <coorhook@gmail.com>
2023-03-24 20:28:49 +0800

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#set -x
+userid=$(id -u)
+if [ ${userid} != 0 ]; then
+        echo "因权限问题不支持--user方式,请选用VUID变量."
+        echo "Due to permission issues, the --user method is not supported. Please use the VUID variable."
+        exit 404
+fi
+# allow the container to be started with `--user`
+if [ ${VUID} == 0 ]; then
+        chown -R nonverysync /data
+        exec /usr/local/bin/gosu nonverysync /usr/bin/verysync -no-browser -home /data/.config -gui-address :${PORT}
+
+else
+        chown -R ${VUID} /data
+        exec /usr/local/bin/gosu ${VUID} /usr/bin/verysync -no-browser -home /data/.config -gui-address :${PORT}
+fi
+
+exec "$@"
